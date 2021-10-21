721f554d28a48370efe927bc139b186d695c72ba
Artem Pikalov <aepikalov@gmail.com>
2021-10-22 01:32:32 +0400

    JavaMultithreading_2_1_Synchronization ver.2

--- src/main/java/Dealer.java
+++ src/main/java/Dealer.java
@@ -4,20 +4,25 @@ import java.util.List;
 public class Dealer {
     private static final int RECIEVE_TIME = 3000;
     private static final int SELL_TIME = 1000;
-    private List<Car> cars = new ArrayList<>();
+    private static final int CARS = 10;
+    private final List<Car> cars = new ArrayList<>();
 
-    public synchronized void receiveCar() {
-        try {
-            Thread.sleep(RECIEVE_TIME);
-            cars.add(new Car());
-            System.out.println(Thread.currentThread().getName() + " выпустил 1 авто.");
-            notify();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    public void receiveCar() {
+        for (int i = 0; i < CARS; i++) {
+            try {
+                Thread.sleep(RECIEVE_TIME);
+                cars.add(new Car());
+                System.out.println(Thread.currentThread().getName() + " выпустил 1 авто.");
+                synchronized (this) {
+                    notify();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    public synchronized Car sellCar() {
+    public synchronized void sellCar() {
         try {
             System.out.println(Thread.currentThread().getName() + " зашел в автосалон.");
             while (cars.size() == 0) {
@@ -26,9 +31,9 @@ public class Dealer {
             }
             Thread.sleep(SELL_TIME);
             System.out.println(Thread.currentThread().getName() + " уехал на новеньком авто.");
+            cars.remove(0);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        return cars.remove(0);
     }
 }
