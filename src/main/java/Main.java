69e3c1abd4097db758a0442e4be60f377e8ead5c
Artem Pikalov <aepikalov@gmail.com>
2021-10-21 10:21:36 +0400

    JavaMultithreading_2_1_Synchronization ver.1.1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,17 @@
 public class Main {
     private static final int CARS = 10;
     public static final int PAUSE = 5000;
+    public static final int BUYERS = 3;
     public static int soldCars;
 
     public static void main(String[] args) {
         final Dealer dealer = new Dealer();
         while (soldCars < CARS) {
-            Thread buyer1 = new Thread(null, dealer::sellCar, "Покупатель 1");
-            Thread buyer2 = new Thread(null, dealer::sellCar, "Покупатель 2");
-            Thread buyer3 = new Thread(null, dealer::sellCar, "Покупатель 3");
-            buyer1.setDaemon(true);
-            buyer1.start();
-            buyer2.setDaemon(true);
-            buyer2.start();
-            buyer3.setDaemon(true);
-            buyer3.start();
+            for (int i = 1; i <= BUYERS; i++) {
+                Thread buyer = new Thread(null, dealer::sellCar, "Покупатель " + i);
+                buyer.setDaemon(true);
+                buyer.start();
+            }
             new Thread(null, dealer::receiveCar, "Производитель").start();
             try {
                 Thread.sleep(PAUSE);
